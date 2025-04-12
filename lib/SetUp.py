96cdaee21aeb7b42af2aea176d1ec2549db99020
Daniel L <lanieldarson@gmail.com>
2025-04-12 10:04:48 -0700

    Formating correction in SetUp.py

--- lib/SetUp.py
+++ lib/SetUp.py
@@ -3,13 +3,13 @@
 # Enter cords in x, y format
 # Ex: url_cords = (459, 97)  
 
-url_cords = (x, y)
+url_cords = (x1, y1)
 
 
 # Presses the down or up arrow on Tiktok to scroll to next video
 # Enter cords in x, y format
 # Ex:button_cords = (883, 503 )
 
-button_cords = (x, y)
+button_cords = (x2, y2)
 
 
